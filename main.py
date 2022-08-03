08c228b624d928b333f86b5343ee0b77eb65db95
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-03 00:07:42 +0000

    changes

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
   print("You can ride roalercoaster!")
+  age = int(input("What is your age?"))
+  if age > 18:
+    print("You need to pay 15$")
+  elif age>12 & age<=18:
+    print("You need to pay 12")
+  else:
+    print("You need to pay 7$")
 else:
   print("You can't ride roalercoaster!!!")
 
-print("Works")
